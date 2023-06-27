685ea686b656e36ea4f25c7a425741ff01aa28bc
M Haidar Hanif <git@mhaidarhanif.com>
2023-06-27 19:41:46 +0700

    display youtubers

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,45 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+const youtubers = [
+  {
+    id: 1,
+    username: "teknobarengpeter",
+    name: "Peter Jack Kambey",
+    bio: "Seorang programmer 1",
+  },
+  {
+    id: 2,
+    username: "webprogrammingunpas",
+    name: "Sandhika Galih",
+    bio: "Seorang programmer 2",
+  },
+  {
+    id: 3,
+    username: "resir014",
+    name: "Resi Respati",
+    bio: "Seorang programmer 3",
+  },
+  {
+    id: 3,
+    username: "zainf",
+    name: "Zain Fathoni",
+    bio: "Seorang programmer 4",
+  },
+];
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>YouTubers</h1>
+      <ul>
+        {youtubers.map((youtuber) => {
+          return (
+            <li>
+              <h2>{youtuber.name}</h2>
+            </li>
+          );
+        })}
+      </ul>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
